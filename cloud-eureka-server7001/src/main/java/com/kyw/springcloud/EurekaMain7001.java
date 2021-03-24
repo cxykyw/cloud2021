ff04ffa77f9a53c6a23c6cba59ff299d174576d0
kangyuwei <kangyuwei@jiuqi.com.cn>
2021-03-24 17:56:33 +0800

    eureka集群搭建

--- cloud-eureka-server7001/src/main/java/com/kyw/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/kyw/springcloud/EurekaMain7001.java
@@ -2,17 +2,17 @@ package com.kyw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author kyw
  * @version 1.0
- * @date 2021/3/23 14:17
+ * @date 2021/3/24 17:11
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
