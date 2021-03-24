a7d536f30e4c0a2d9991388434b6a4322e1a279d
kyw <1337441734@qq.com>
2021-03-24 20:30:38 +0800

    多服务集群

---
+++ cloud-provider-payment8002/src/main/java/com/kyw/springcloud/PaymentMain8002.java
@@ -0,0 +1,19 @@
+package com.kyw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname PaymentMain8002
+ * @Description TODO
+ * @Date 2021/3/24 20:18
+ * @Created by kyw
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class,args);
+    }
+}
---
+++ cloud-provider-payment8002/src/main/java/com/kyw/springcloud/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.kyw.springcloud.service;
+
+import com.kyw.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @Classname PaymentService
+ * @Description TODO
+ * @Date 2021/3/22 22:54
+ * @Created by kyw
+ */
+public interface PaymentService {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+}
