ffa1a970f7cd679365135922371800d115a8ffc5
kangyuwei <kangyuwei@jiuqi.com.cn>
2021-04-02 09:13:01 +0800

    Hystrix和Feign

--- cloud-provider-hystrix-payment8001/src/main/java/com/kyw/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/kyw/springcloud/controller/PaymentController.java
@@ -1,9 +1,39 @@
 package com.kyw.springcloud.controller;
 
+import com.kyw.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author kyw
  * @version 1.0
  * @date 2021/3/31 17:35
  */
+@RestController
+@Slf4j
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentOK(@PathVariable("id")Integer id){
+        String s = paymentService.paymentInfo_OK(id);
+        log.info("########result:"+s);
+        return s;
+    }
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentTimeout(@PathVariable("id")Integer id){
+        String s = paymentService.paymentInfo_TimeOut(id);
+        log.info("########result:"+s);
+        return s;
+    }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/kyw/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/kyw/springcloud/service/PaymentService.java
@@ -2,6 +2,8 @@ package com.kyw.springcloud.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author kyw
  * @version 1.0
@@ -12,5 +14,15 @@ public class PaymentService {
 
     public String paymentInfo_OK(Integer id){
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK,id:"+id+"\t"+"(#^.^#)";
+    }
+
+    public String paymentInfo_TimeOut(Integer id){
+        int timeout = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK,id:"+id+"\t"+"(#^.^#),耗时"+timeout+"秒";
     }
 }
