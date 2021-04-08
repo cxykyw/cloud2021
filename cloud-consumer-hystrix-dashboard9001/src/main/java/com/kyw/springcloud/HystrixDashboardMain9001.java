39886e2e499650d1c3a61c236a68eef5c6070617
kangyuwei <kangyuwei@jiuqi.com.cn>
2021-04-08 20:24:49 +0800

    Hystrix熔断和图形化面板

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/kyw/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.kyw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author kyw
+ * @version 1.0
+ * @date 2021/4/8 16:42
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
